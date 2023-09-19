5e48e38e47dcd80466f8ed892d3db9b9013dec79
Asdmitruk <dmitrukadrian@gmail.com>
2023-09-19 03:07:53 +0300

    add_fix_task

--- page/main/task2/task2.js
+++ page/main/task2/task2.js
@@ -1,39 +1,31 @@
 class CustomTooltip extends HTMLElement {
+  static observedAttributes = ["text"];
+
+  get text() {
+    return this.getAttribute("text");
+  }
+
+  set text(value) {
+    this.setAttribute("text", value);
+  }
+
   connectedCallback() {
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-          <style>
-            .custom-tooltip {
-              position: relative;
-              display: inline-block;
-            }
-        
-            .text {
-              visibility: hidden;
-              background-color: #333;
-              color: #fff;
-              text-align: center;
-              border-radius: 4px;
-              padding: 6px;
-              position: absolute;
-              z-index: 1;
-              bottom: 125%;   
-              left: 50%;
-              transform: translateX(-50%);
-              opacity: 0;
-              transition: opacity 0.3s, visibility 0.3s;
-            }
-        
-            .custom-tooltip:hover .text {
-              visibility: visible;
-              opacity: 1;
-            }
-          </style>
-          <div class="custom-tooltip">
-              <slot></slot>
-              <div class="text">${this.getAttribute("text")}</div>
-          </div>
-      `;
+    this.updateTooltip();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "text" && oldValue !== newValue) {
+      this.updateTooltip();
+    }
+  }
+
+  updateTooltip() {
+    this.innerHTML = `  
+      <div class="custom-tooltip">
+        <slot></slot>
+        <div class="text">${this.text}</div>
+      </div>
+    `;
   }
 }
 
--- page/main/task3/task3.js
+++ page/main/task3/task3.js
@@ -1,26 +1,24 @@
 class CustomTextarea extends HTMLElement {
-  connectedCallback() {
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.innerHTML = `
-          <style>
-            .custom-textarea {
-              width: 100%;
-              padding: 10px;
-              border: 1px solid #ccc;
-              resize: none;
-              font-size: 16px;
-              min-height: 40px;
-              box-sizing: border-box;
-              border-radius: 8px;
-            }
-          </style>
-          <textarea class="custom-textarea"></textarea>
-      `;
+  static observedAttributes = ["value"];
 
-    const textarea = this.shadowRoot.querySelector(".custom-textarea");
+  get value() {
+    return this.getAttribute("value") || "";
+  }
+
+  set value(newValue) {
+    this.setAttribute("value", newValue);
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <textarea class="custom-textarea">${this.value}</textarea>
+    `;
+
+    const textarea = this.querySelector(".custom-textarea");
 
     textarea.addEventListener("input", () => {
       this.adjustTextareaHeight(textarea);
+      this.value = textarea.value;
     });
 
     this.adjustTextareaHeight(textarea);
@@ -29,6 +27,14 @@ class CustomTextarea extends HTMLElement {
   adjustTextareaHeight(textarea) {
     textarea.style.height = "auto";
     textarea.style.height = textarea.scrollHeight + "px";
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "value" && oldValue !== newValue) {
+      const textarea = this.querySelector(".custom-textarea");
+      textarea.value = newValue || "";
+      this.adjustTextareaHeight(textarea);
+    }
   }
 }
 
